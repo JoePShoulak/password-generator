a0ff69726f68707e2dbbcba0cbff8480b1ce1108
Joe P Shoulak <joe@squarecompass.io>
2022-09-15 14:31:40 -0500

    better commenting

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Query the user for the length of the password
 function getUserLength() {
   return parseInt(prompt("How many characters?"));
 }
 
+// Get a random number for use with selecting the new password character from the character pool
 function randomNumber(max) {
   return Math.floor(Math.random()*max);
-}
-
-function generatePassword() {
-  var passLength = getUserLength();
-
-  var lowerCase = useLowerCase();
-  var upperCase = useUpperCase();
-  var numbers = useNumbers();
-  var special = useSpecial();
-
-  var password = "";
-  var charpool = [];
-  if (lowerCase) {charpool.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")}
-  if (upperCase) {charpool.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")}
-  if (numbers) {charpool.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")}
-  if (special) {charpool.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", ",", ".", "/", "<", ">", "?", ":", ";", "|", "{", "}", "[", "]", "`", "~", "'", "\"", "\\")}
-
-  for (i=0; i<passLength; i++) {
-    password += charpool[randomNumber(charpool.length)];
-  }
-
-  return password;
 }
 
 function useLowerCase() {
@@ -45,6 +25,36 @@ function useNumbers() {
 
 function useSpecial() {
   return confirm("Use special characters?");
+}
+
+// Actually generate the password
+function generatePassword() {
+  // Get the length from the user
+  var passLength = getUserLength();
+
+  // Ask the user which character sets we're going to use
+  var lowerCase = useLowerCase();
+  var upperCase = useUpperCase();
+  var numbers = useNumbers();
+  var special = useSpecial();
+
+  // prepare holder variables
+  var password = "";
+  var charpool = [];
+
+  // If we're using the character set, add that character set to our pool
+  if (lowerCase) {charpool.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")}
+  if (upperCase) {charpool.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")}
+  if (numbers) {charpool.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")}
+  if (special) {charpool.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", ",", ".", "/", "<", ">", "?", ":", ";", "|", "{", "}", "[", "]", "`", "~", "'", "\"", "\\")}
+
+  // For each character in the pass length, select a random character from the pool and add it to the password
+  for (i=0; i<passLength; i++) {
+    password += charpool[randomNumber(charpool.length)];
+  }
+
+  // Bring it home
+  return password;
 }
 
 // Write password to the #password input
