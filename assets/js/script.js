e90fcf779d62217159e03399736ab3fd73d01000
Joe P Shoulak <joe@squarecompass.io>
2022-10-16 22:12:08 -0500

    polish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
 // Generate the password
 function generatePassword() {  
   // Get the length from the user and validate on password length (between 8 and 128)
-  var passLength = getUserLength();
+  let password = "";
+  
+  const passLength = getUserLength();
+
   if (passLength < 8 || passLength > 128) {
     alert("Password must be between 8 and 128 characters.");
-    return "";
+    return password;
   }
 
   // Get the character pool from the user and validate character pool (not empty)
-  var charPool = getCharPool();
+  const charPool = getCharPool();
+  
   if (charPool.length == 0) {
     alert("You must choose at least one character set for your password.");
-    return "";
+    return password;
   }
 
   // For each character in the pass length, select a random character from the pool and add it to the password
-  var password = "";
   for (i=0; i<passLength; i++) {
     password += randomElement(charPool);
   }
